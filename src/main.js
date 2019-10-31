27c55b70174e92b3378ab8e561cdcf505a25c203
sergio.gordo <sergio@meta-site.com>
2019-10-31 05:42:50 +0100

    drift

--- src/main.js
+++ src/main.js
@@ -5,6 +5,26 @@ import "~/assets/styles/scss/style.scss";
 import "~/assets/styles/scss/_normalize.scss";
 // import "~/assets/js/main.js";
 
-export default function (Vue) {
+export default function (Vue, { router, head, isClient, appOptions }) {
+  
+  // Add an external CSS file
+  /*  head.link.push({
+      rel: 'stylesheet',
+      href: 'https://some-server.com/external-styleheet.css'
+    });*/
+  
+  // Add an external Javascript before the closing </body> tag
+  head.script.push({
+    src: '/js/drift.js',
+    body: true
+  });
+  
+  // Add a meta tag
+  /*  head.meta.push({
+      name: 'keywords',
+      content: 'HTML,CSS,XML,JavaScript'
+    });*/
+  
   Vue.component('Layout', DefaultLayout)
 }
+
