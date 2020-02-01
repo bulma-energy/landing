fb408c3e68a6ea7b79c2e39872f7fdf62bb67115
sergio <sergio@MacBook-Pro-de-Federico.local>
2020-02-02 00:14:17 +0100

    design updates

---
+++ src/components/Features.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Features"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Newsletter.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Newsletter"
+    }
+</script>
+
+<style scoped>
+
+</style>
